034d3bccd34caa6e23bcaf2ce1262cde8abc477c
Jaime Ashander <jashander@ucdavis.edu>
2014-01-14 14:27:32 -0800

    Function for opening subs, writing to file on exit, better cl try/catch

--- grading.py
+++ grading.py
@@ -5,6 +5,7 @@ import sys
 import re
 import string
 from subprocess import call
+import itertools
 
 re_studentid = re.compile(r"[0-9]{7,9}")
 
@@ -21,15 +22,46 @@ def matchdir (string, dirlist):
     else:
         return outlist.pop()
     
+def openpdfanddoc(listoffiles):
+    ''' open pdf and doc files in teh submission directory'''
+    filestoopen  = [f for f in listoffiles if f.find('.pdf') != -1 or f.find('.doc') != -1]
+    for f in filestoopen:
+        filepath = os.path.join(os.getcwd(), submissionsdir, f)
+        if sys.platform.startswith('darwin'):
+            call(('open', filepath))
+        elif os.name == 'nt':
+            os.startfile(filepath)
+        elif os.name == 'posix':
+            call(('xdg-open', filepath))
+
 
 def main():
+    '''
+    python grading.py [infile] [gradefile] [hwname] [gradername] [exitfile='notgraded.csv']
+    '''
+    # [todo] - should replace below with argparser module
     try:
-         hwname = sys.argv[1]
-         gradername= sys.argv[2]
-         writefile = sys.argv[3]
+        infile=sys.argv[1]
     except:
-         hwname = 'Comments on Homework ___ '
-         gradername = os.uname()[1]
+        infile = 'grades.csv'
+    try:
+        gradefile = sys.argv[2]
+    except:
+        gradefile = infile+'.bak'
+        print "graded output will be put in grades.csv.bak\n"
+    try:
+        hwname = sys.argv[3]
+    except:
+        hwname = 'Comments on Homework ___ '
+
+    try:
+        gradername= sys.argv[4]
+    except:
+        gradername = os.uname()[1]
+    try:
+        exitfile=sys.argv[5]
+    except:
+        exitfile='notgraded.csv'
 
     cwd = os.getcwd()
     student_dirs = os.listdir(cwd)
@@ -37,29 +69,28 @@ def main():
     EDITOR = os.getenv('EDITOR')
     COMMENTFILE = 'comments.txt'
     
-    f = open('grades.csv','r')
-    fout = open(writefile,'w')
-    for l in f:
+    f = open(infile,'r')
+    tograde = f.readlines()
+    f.close()
+
+    fout = open(gradefile,'w')
+    f2 = open(exitfile, 'w')
+
+    keepgoing = True
+    for l in tograde:
         ID = re_studentid.search(l,)
-        fout.writelines(l.rstrip()) 
-        if ID:
+        if keepgoing:
+            fout.writelines(l.rstrip())
+        if (not keepgoing) or (not ID):
+            f2.writelines(l)
+        if ID and keepgoing:
             curstudent = matchdir(ID.group(), student_dirs)
             os.chdir(curstudent)
 
-            dircont = os.listdir(os.getcwd())
-            submissionsdir = matchdir('Submission', dircont)
-            subs = os.listdir(submissionsdir)
-            
-            filestoopen  = [f for f in subs if f.find('.pdf') != -1 or f.find('.doc') != -1]
-            for f in filestoopen:
-                filepath = os.path.join(os.getcwd(), submissionsdir, f)
-                if sys.platform.startswith('darwin'):
-                    call(('open', filepath))
-                elif os.name == 'nt':
-                    os.startfile(filepath)
-                elif os.name == 'posix':
-                    call(('xdg-open', filepath))
+            submissionsdir = matchdir('Submission', os.listdir(os.getcwd()))
+            subdirfiles = os.listdir(submissionsdir)
 
+            openpdfanddoc(subdirfiles)
             
             comment_header = "Comments on " + hwname + " for " + curstudent 
             comment_footer = "- graded by " + gradername
@@ -90,18 +121,29 @@ def main():
             editit = raw_input("Would you like to edit? Enter y/Y if so.\n")
             if editit == 'Y' or editit == 'y':
                 call([EDITOR, COMMENTFILE])
-                grade = raw_input("Did you change the grade? If so enter it X/5:\n")
-                while float(grade) > 5.0:
-                    grade = raw_input("Grade must be less than 5. What is the grade X/5?\n")
+                grade = raw_input("Did you change the grade? If so enter it below X/5. If not hit enter.\n")
+                if grade != '':
+                    while float(grade) > 5.0:
+                        grade = raw_input("Grade must be less than 5. What is the grade X/5?\n")
             print '\n'
             print '\n'
             os.chdir('..')
             fout.writelines(grade)
-            
-        fout.writelines('\n')
-    fout.flush()
-    fout.close()
-    f.close()
+            fout.writelines('\n')
+            escape = raw_input("To keep going hit enter or another key.\nTo quit and save ungraded files, press Q/q:")
+            if (escape == 'Q') or (escape == 'q'):
+                keepgoing = False
+                fout.flush()
+                fout.close()
+    if keepgoing:
+        fout.flush()
+        fout.close()
+    if not keepgoing:
+        f2.flush()
+        f2.close()
+                
+        
+
 
         
 if __name__=='__main__':
